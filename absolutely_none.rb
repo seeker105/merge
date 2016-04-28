b3ca2157d8f62ba97c23e581b213ca1d5c224cb1
Christopher Soden <karavol27@gmail.com>
2016-04-28 14:48:44 -0600

    temp commit

--- absolutely_none.rb
+++ absolutely_none.rb
@@ -1,7 +1,9 @@
 class AbsolutelyNone
 
-  def method1(no_youre_an_argument)
+  def method1(hey_im_a_parameter)
     and_im_a_string = "wow!"
+    "this is"
+    "Really crazy"
     puts hey_im_a_parameter
     hey_im_a_parameter = hey_im_a_parameter + and_im_a_string
   end
