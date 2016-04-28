db58f6263f26ab58e206f85dbbd22bc0ad6c2af5
Christopher Soden <karavol27@gmail.com>
2016-04-28 16:31:22 -0600

    c

--- absolutely_none.rb
+++ absolutely_none.rb
@@ -3,12 +3,12 @@ class AbsolutelyNone
   def method1(hey_im_a_parameter)
     and_im_a_string = "wow!"
     puts when_are_we
-    hey_im_a_pgetesr = hey_im_a_parameter + and_im_a_string
+    hey_im_atesr = hey_im_a_parameter + and_im_a_string
     "no, padawan"
     "Crazy, this is!"
   end
 
-  def method2s
+  def mod2s
 
   end
 
