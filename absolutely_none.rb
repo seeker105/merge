583ee5b574609e16bfdf1fb1ceb81e1054d464a0
Christopher Soden <karavol27@gmail.com>
2016-04-28 14:34:56 -0600

    Initial commit

---
+++ absolutely_none.rb
@@ -0,0 +1,14 @@
+class AbsolutelyNone
+
+  def method1(hey_im_a_parameter)
+    and_im_a_string = "wow!"
+    puts hey_im_a_parameter
+    hey_im_a_parameter = hey_im_a_parameter + and_im_a_string
+  end
+
+  def method2
+
+  end
+
+
+end
