59ffd28c8887545177c2261a66404620875b463d
Christopher Soden <karavol27@gmail.com>
2016-04-28 14:44:29 -0600

    change var name

--- absolutely_none.rb
+++ absolutely_none.rb
@@ -1,6 +1,6 @@
 class AbsolutelyNone
 
-  def method1(hey_im_a_parameter)
+  def method1(no_youre_an_argument)
     and_im_a_string = "wow!"
     puts hey_im_a_parameter
     hey_im_a_parameter = hey_im_a_parameter + and_im_a_string
