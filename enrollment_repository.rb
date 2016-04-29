2b580276d4bac0efe9c92034668db803164edb49
Christopher Soden <karavol27@gmail.com>
2016-04-28 22:27:00 -0600

    c

--- enrollment_repository.rb
+++ enrollment_repository.rb
@@ -5,13 +5,9 @@ class EnrollmentRepository
   attr_reader :data, :enrollments
 
   def load_data(data)
-    "this is nextbranch"
     data_manager = DataManager.new
     data_manager.load_data(data)
     populate_repo_yes(data_manager.create_enrollments)
-    "no padawan"
-    "what do u think?"
-    "crazy this is"
   end
 
   def populate_repo(raw_enrollments)
