27d39c52bac48f0379137a691b0da2cac43d7e07
Christopher Soden <karavol27@gmail.com>
2016-05-02 19:05:49 -0600

    c

--- enrollment.rb
+++ enrollment.rb
@@ -1,7 +1,7 @@
 require 'pry'
 require_relative '../lib/clean_data'
 
-class Enrollment
+class EnrollmentOther
   include CleanData
 
   attr_reader :name, :kindergarten_participation, :high_school_graduation
