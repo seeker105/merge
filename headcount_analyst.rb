bc4af45d867cdf1d90af87c3d3da3445d1dcccac
Christopher Soden <karavol27@gmail.com>
2016-05-02 19:06:11 -0600

    c

--- headcount_analyst.rb
+++ headcount_analyst.rb
@@ -3,7 +3,7 @@ require 'csv'
 require_relative '../lib/clean_data'
 require_relative '../lib/statewide_categories'
 
-class HeadcountAnalyst
+class HeadcountAnalystNext
   include CleanData
   include StatewideCategories
 
